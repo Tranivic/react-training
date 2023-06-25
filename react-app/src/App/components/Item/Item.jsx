01bc45ace2e81f0624320fd4c46df935366d3a74
Tranivic <victor.trani1@gmail.com>
2023-06-25 12:37:19 -0300

    fix: propTypes running in children prop at Item component

--- react-app/src/App/components/Item/Item.jsx
+++ react-app/src/App/components/Item/Item.jsx
@@ -1,11 +1,15 @@
 import "./Item.css";
+import PropTypes from "prop-types";
 
-function Item(children) {
+function Item(props) {
   return (
     <div className="item-component">
-      <h1>{children.children}</h1>
+      <h1>{props.children}</h1>
     </div>
   );
 }
+Item.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default Item;
