54787728e44e7f40a362aed9e009f11e3d5ad124
Tranivic <victor.trani1@gmail.com>
2023-06-26 18:27:37 -0300

    working with events

--- react-app/src/App/components/FunctionBased/FunctionBased.jsx
+++ react-app/src/App/components/FunctionBased/FunctionBased.jsx
@@ -1,26 +1,42 @@
 import { useState, useRef } from "react";
 
 function FunctionBased() {
-  const [stateBased, setStateBased] = useState({
+  const [data, setData] = useState({
     content: "This is the useState",
+    duplicateInput: "",
   });
   const refBased = useRef("This is the useRef");
 
   const handleClick = (type) => {
     if (type === "state") {
-      setStateBased({ content: "State updated" });
+      setData({ content: "State updated" });
     }
     if (type === "ref") {
       refBased.current = "Ref updated";
     }
   };
 
+  const duplicateInput = (recivedEvent) => {
+    const newValue = recivedEvent.nativeEvent.data;
+    const lastValue = data.duplicateInput;
+
+    if (newValue !== null) {
+      setData((prevState) => ({
+        ...prevState,
+        duplicateInput: lastValue + newValue,
+      }));
+    }
+    console.log(recivedEvent);
+  };
+
   return (
     <>
-      <h1>UseState: {stateBased.content}</h1>
+      <h1>UseState: {data.content}</h1>
       <h1>UseRef: {refBased.current}</h1>
       <button onClick={() => handleClick("state")}>Change State</button>
       <button onClick={() => handleClick("ref")}>Change Ref</button>
+      <input onInput={(event) => duplicateInput(event)} type="text" />
+      <p className="input-text">{data.duplicateInput}</p>
     </>
   );
 }
